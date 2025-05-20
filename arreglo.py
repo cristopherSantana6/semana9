1a55393805c62f856fa585088e4ff3189c91b16e
cristopherSantana6 <cristopherrafael644@gmail.com>
2025-05-20 08:38:43 -0600

    arreglo

---
+++ arreglo.py
@@ -0,0 +1,14 @@
+array_int = {1, 2, 3, 4, 5}
+array_float = {1.0, 2.0, 3.0, 4.0, 5.0}
+array_str = {"uno", "dos", "tres", "cuatro", "cinco"}
+array_char = {"a", "b", "c", "d", "e"}
+array_bool = {True, False, True, False, True}
+arrat_mixed = {1, 2.0, "tres", "cuatro", True, False}
+
+#imprimir los arrays
+print("numeros enteros: ", array_int)
+print("numeros decimales: ", array_float)
+print("strings: ", array_str)
+print("caracteres: ", array_char)
+print("booleanos: ", array_bool)
+print("mezcla: ", arrat_mixed)
